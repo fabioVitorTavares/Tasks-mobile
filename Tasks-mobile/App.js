e7da3a323e0133c6f46789d03409e8718f90f073
fabioVitoTavares <fabio.furtado@estudante.ufla.br>
2023-01-05 00:59:46 -0300

    commit 050120230059

---
+++ Tasks-mobile/App.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Task } from './components/task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Text>Tasks!</Text>
+      <ScrollView>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+        <Task/>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+});
---
+++ Tasks-mobile/components/task/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+export function Task() {
+  return (
+    <Text style={styles.task}>
+      Tarefa {Math.random()}
+    </Text>
+  )
+};
+
+const styles = StyleSheet.create({
+  task: {
+    flex: 1,
+    color: '#52f3g2',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 25,
+  },
+});
