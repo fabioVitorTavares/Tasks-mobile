dcf0e74f395934bf809fb58018f8b79237f1d36d
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-02-07 23:07:32 -0300

    commit 070220232307

--- Tasks-mobile/components/Calendar/index.tsx
+++ Tasks-mobile/components/Calendar/index.tsx
@@ -1,11 +1,33 @@
-import { View, Text } from "react-native"
-import { styles } from "./style"
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { styles } from "./style";
 
 export function Calendar() {
+  const week: String[] = ["D", "S", "T", "Q", "Q", "S", "S"];
+  const [numbers, setNumbers] = useState<number[]>([]);
+  const [date, setDate] = useState<Date>(new Date());
+
+  useEffect(() => {
+    setNumbers([
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4,
+      5, 6, 7, 8, 9, 10,
+    ]);
+  }, []);
+
+  useEffect(() => {
+    console.log(date.toString());
+  }, [date]);
 
   return (
     <View style={styles.calendar}>
-      <Text>Calendar</Text>
+      {week.map((day) => (
+        <Text style={styles.cells}>{day}</Text>
+      ))}
+      {numbers.map((number) => (
+        <TouchableOpacity onPress={() => setDate(new Date(2023, 1, number))}>
+          <Text style={styles.cells}>{String(number)}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
-  )
+  );
 }
--- Tasks-mobile/components/Calendar/style.ts
+++ Tasks-mobile/components/Calendar/style.ts
@@ -3,6 +3,17 @@ export const styles = StyleSheet.create({
   calendar: {
     width: 350,
     height: 300,
-    backgroundColor: 'violet'
+    backgroundColor: '#f55',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  cells: {
+    width: 45,
+    height: 45,
+    margin: 2.5,
+
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderWidth: 0.4,
   }
 })
