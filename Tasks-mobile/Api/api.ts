796f23448ce54548b32d906d2a41703802481100
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-03-20 22:24:11 -0300

    commit 200320232223

---
+++ Tasks-mobile/Api/api.ts
@@ -0,0 +1,18 @@
+import  axios  from "axios";
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Content-Type": "application/json",
+};
+
+const baseUrl = 'http://10.0.2.2:8080'
+
+
+
+export async function getAllTasks() {
+  try {    
+    return await axios.get(`${baseUrl}/task/getAllTasks`)
+  } catch (error) {
+    console.log('Erro:',error);
+  }
+}
