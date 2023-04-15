8e23cf2499033be632ad296fa0da88f5b594fba5
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-04-15 12:52:36 -0300

    commit

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,18 +1,9 @@
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { Teste } from "../../teste";
 import { Login } from "../Screens/Login/login";
 
 export function Routes() {
-  const Stack = createStackNavigator();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Teste" component={Teste} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Login/>
   );
 }
