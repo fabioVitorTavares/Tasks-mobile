4dc52964cc6eff9ffe97b144412aab758f5e5423
fabioVitorTavares <fabio.furtado@estudante.ufla.br>
2023-05-05 00:24:24 -0300

    commit

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -1,24 +1,39 @@
-import { View, Text } from "react-native";
-import {
-  BackgroundApp,
-  TitleHome,
-  ContentHome,
-  CardMenu,
-  styles,
-} from "./styles";
+import Styles from "./styles";
+import { View, Text, TouchableOpacity } from "react-native";
+import { FontAwesome5 as Icon } from "@expo/vector-icons";
+import { Ionicons as Icon2 } from "@expo/vector-icons";
+import { FontAwesome as Icon3 } from "@expo/vector-icons";
 
 export default function Home() {
   return (
-    <View style={styles.backgroundApp}>
-      <TitleHome>Tarefas</TitleHome>
-      <ContentHome>
-        <CardMenu></CardMenu>
-        <CardMenu></CardMenu>
-        <CardMenu></CardMenu>
-        <CardMenu></CardMenu>
-        <CardMenu></CardMenu>
-        <CardMenu></CardMenu>
-      </ContentHome>
+    <View style={Styles.backgroundApp}>
+      <Text style={Styles.titleHome}>Tarefas</Text>
+      <View style={Styles.contentHome}>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon name="tasks" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Tarefas</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon2 name="today-sharp" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Agenda</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon2 name="notifications" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Notificações</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon name="tools" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Executar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon3 name="gears" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Config</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={Styles.cardMenu}>
+          <Icon name="user-edit" size={100} color="#fff" />
+          <Text style={Styles.titleCardMenu}>Usuário</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -17,53 +17,44 @@ const { dfc, wh100 } = StyleSheet.create({
 
 
 
-export const styles = StyleSheet.create({
+const Styles = StyleSheet.create({
   backgroundApp: {
     ...dfc,
     ...wh100,
+    paddingHorizontal: 20,
+    paddingVertical: 40
+  },
+  contentHome: {
+    ...dfc,
+    ...wh100,
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "column",
+  },
+  titleHome: {
+    width: "100%",
+    textAlign: "center",
+    fontFamily: "FiraCodeRegular", 
+    fontSize: 30,
+    backgroundColor: "#035",
+    color: "#fff",
+  },
+  cardMenu: {
+    ...dfc,
+    width:" 45%",
+    height: " 30%",
+    margin: "2.5%",
+    borderRadius: 10,
+    backgroundColor: "#0005",
+  },
+  titleCardMenu: {
+    color: "#fff",
+    fontFamily: "FiraCodeRegular",
+    fontWeight: "bold",
+    fontSize: 26
   }
 
 });
-
-
-export const BackgroundApp = styled.View`
-  ${displayFlexCenter}
-  width: 100%;
-  height: 100%;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  padding-left: 20px;
-  padding-right: 20px;
-`;
-
-export const ContentHome = styled.View`
-  ${displayFlexCenter}
-  flex: 1;
-  flex-wrap: wrap;
-  flex-direction: column;
-  width: 100%;
-  height: 100%;
-`;
-
-export const CardMenu = styled.View`
-  width: 45%;
-  height: 30%;
-  margin-left: 2.5%;
-  margin-right: 2.5%;
-  margin-top: 2.5%;
-  margin-bottom: 2.5%;
-  border-width: 1px;
-  border-radius: 10px;
-  background-color: #0009;
-`;
-
-export const TitleHome = styled.Text`
-  width: 100%;
-  text-align: center;
-  font-family: 'FiraCodeRegular'; 
-  font-size: 30px;
-  background-color: #035;
-  color: #fff;
-`;
-
+ 
+export default Styles;
 
